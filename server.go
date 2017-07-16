35bcedc489017eb4b571242f6013dce056813a0b
Uriel Avalos <uriel@rstudio.com>
2017-07-15 23:35:57 -0600

    config file exists

--- server.go
+++ server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"github.com/frankandrobot/acadia/file"
 	"github.com/frankandrobot/acadia/messaging"
 	"github.com/frankandrobot/acadia/queue"
 	"github.com/labstack/echo"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 // CONFIG
@@ -14,22 +18,42 @@ type ServerRoot string
 type ServerKey string
 type ServerCer string
 
-var serverRoot = ServerRoot("/home/pi/data")
-var serverKey = ServerKey("/home/pi/server.key")
-var serverCert = ServerCer("/home/pi/server.crt")
+type config struct {
+	ServerRoot *ServerRoot `json:"serverRoot"`
+	ServerKey  *ServerKey  `json:"serverKey"`
+	ServerCert *ServerCer  `json:"serverCert"`
+}
 
 type contents struct {
 	Contents string `form:"contents" json:"contents" binding:"required"`
 }
 
 func main() {
+	rawConfig, err := ioutil.ReadFile(os.Getenv("HOME") + "/.acadia.json")
+	if err != nil {
+		panic(err)
+	}
+	var config config
+	err = json.Unmarshal(rawConfig, &config)
+	if err != nil {
+		panic(err)
+	}
+	if config.ServerRoot == nil {
+		panic(errors.New("Need a server root"))
+	}
+	if config.ServerKey == nil {
+		panic(errors.New("Need a server key"))
+	}
+	if config.ServerCert == nil {
+		panic(errors.New("Need a server certificate"))
+	}
 	router := echo.New()
 	queue := queue.MakeQueue()
 	fileIO := file.NewFileIO()
 
 	router.GET("/files", func(c echo.Context) error {
 		action := func() messaging.ChanResult {
-			files, err := fileIO.LoadDir(string(serverRoot))
+			files, err := fileIO.LoadDir(string(*config.ServerRoot))
 			result := messaging.ChanResult{}
 			result.Filenames = files
 			result.Error = err
@@ -46,7 +70,7 @@ func main() {
 		name := c.Param("name")
 		action := func() messaging.ChanResult {
 			contents, err := fileIO.LoadFile(
-				file.Root(serverRoot),
+				file.Root(*config.ServerRoot),
 				file.BaseFilename(name),
 			)
 			result := messaging.ChanResult{}
@@ -69,7 +93,7 @@ func main() {
 		}
 		action := func() messaging.ChanResult {
 			err := fileIO.SaveFile(
-				file.Root(serverRoot),
+				file.Root(*config.ServerRoot),
 				file.BaseFilename(name),
 				file.Contents(doc.Contents),
 			)
@@ -84,5 +108,8 @@ func main() {
 		return c.JSON(http.StatusOK, doc)
 	})
 
-	router.StartTLS(":8080", string(serverCert), string(serverKey))
+	err = router.StartTLS(":8080", string(*config.ServerCert), string(*config.ServerKey))
+	if err != nil {
+		panic(err)
+	}
 }
